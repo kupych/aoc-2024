08fae95ae0507fba4c422bcdf7fc419934a2ee88
Stefan Kupych <stefan@hifyre.com>
2024-12-01 00:23:05 -0500

    Day 1

--- lib/day1.ex
+++ lib/day1.ex
@@ -10,16 +10,33 @@ defmodule Aoc.Day1 do
   def day(), do: 1
 
   @impl Day
-  def a(lines) do
-    lines
-    |> Enum.map(&get_calibration_value/1)
+  def a(numbers) do
+    numbers
+    |> unzip_zip()
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
     |> Enum.sum()
   end
 
   @impl Day
-  def b(lines) do
-    lines
-    |> Enum.map(&get_calibration_value_with_words/1)
+  def b(nums) do
+    [l, r] =
+      nums
+      |> Enum.unzip()
+      |> Tuple.to_list()
+
+    acc =
+      l
+      |> Enum.map(&{&1, 0})
+      |> Enum.into(%{})
+
+    occurrences =
+      r
+      |> Enum.reduce(acc, fn num, acc ->
+        Map.update(acc, num, 1, fn x -> x + num end)
+      end)
+
+    l
+    |> Enum.map(&Map.get(occurrences, &1))
     |> Enum.sum()
   end
 
@@ -29,43 +46,22 @@ defmodule Aoc.Day1 do
       file
       |> String.trim()
       |> String.split("\n")
+      |> Enum.map(&parse_nums/1)
     end
   end
 
-  defp get_calibration_value(line) do
-    ~r/\d/
-    |> Regex.scan(line)
-    |> get_first_and_last()
-    |> Enum.join("")
-    |> String.to_integer()
+  defp parse_nums(line) do
+    line
+    |> String.split(~r/\s+/, trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
   end
 
-  defp get_calibration_value_with_words(line) do
-    ~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
-    |> Regex.scan(line)
-    |> Enum.flat_map(&tl/1)
-    |> get_first_and_last()
-    |> Enum.map(&convert_to_number/1)
-    |> Enum.join("")
-    |> String.to_integer()
+  defp unzip_zip(numbers) do
+    numbers
+    |> Enum.unzip()
+    |> Tuple.to_list()
+    |> Enum.map(&Enum.sort/1)
+    |> Enum.zip()
   end
-
-  defp get_first_and_last([digit]) do
-    [digit, digit]
-  end
-
-  defp get_first_and_last([_ | _] = digits) do
-    Enum.take_every(digits, Enum.count(digits) - 1)
-  end
-
-  defp convert_to_number("one"), do: "1"
-  defp convert_to_number("two"), do: "2"
-  defp convert_to_number("three"), do: "3"
-  defp convert_to_number("four"), do: "4"
-  defp convert_to_number("five"), do: "5"
-  defp convert_to_number("six"), do: "6"
-  defp convert_to_number("seven"), do: "7"
-  defp convert_to_number("eight"), do: "8"
-  defp convert_to_number("nine"), do: "9"
-  defp convert_to_number(digit), do: digit
 end
--- lib/day2.ex
+++ lib/day2.ex
@@ -10,18 +10,13 @@ defmodule Aoc.Day2 do
   def day(), do: 2
 
   @impl Day
-  def a(games) do
-    games
-    |> Enum.filter(&valid_game?(&1, %{blue: 14, green: 13, red: 12}))
-    |> Enum.map(& &1.number)
-    |> Enum.sum()
+  def a(_) do
+    :not_solved
   end
 
   @impl Day
-  def b(games) do
-    games
-    |> Enum.map(&(&1.blue * &1.green * &1.red))
-    |> Enum.sum()
+  def b(_) do
+    :not_solved
   end
 
   @impl Day
@@ -29,46 +24,6 @@ defmodule Aoc.Day2 do
     with {:ok, file} <- Day.load(__MODULE__) do
       file
       |> String.split("\n", trim: true)
-      |> Enum.map(&parse_game/1)
     end
-  end
-
-  defp parse_game(line) do
-    [text, number, instructions] = Regex.run(~r/Game (\d+): (.+)/, line)
-
-    %{
-      blue: 0,
-      green: 0,
-      red: 0,
-      text: text,
-      number: String.to_integer(number),
-      instructions: String.split(instructions, "; ", trim: true)
-    }
-    |> run_game()
-  end
-
-  defp run_game(%{instructions: instructions} = game) do
-    Enum.reduce(instructions, game, &do_run_game/2)
-  end
-
-  defp do_run_game(round, game) do
-    round
-    |> String.split(", ", trim: true)
-    |> Enum.reduce(game, &do_run_instruction/2)
-  end
-
-  defp do_run_instruction(instruction, game) do
-    [number, color] = String.split(instruction, " ", trim: true)
-    number = String.to_integer(number)
-    color = String.to_existing_atom(color)
-    Map.update!(game, color, &max(&1, number))
-  end
-
-  defp valid_game?(%{blue: blue, green: green, red: red}, %{
-         blue: blue_max,
-         green: green_max,
-         red: red_max
-       }) do
-    blue <= blue_max and green <= green_max and red <= red_max
   end
 end
