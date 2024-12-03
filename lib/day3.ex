eb6c24fee8531af9a2e578a3e05a9cf2e75edc97
Stefan Kupych <stefan@hifyre.com>
2024-12-03 10:18:12 -0500

    Day 3

--- lib/day3.ex
+++ lib/day3.ex
@@ -10,20 +10,33 @@ defmodule Aoc.Day3 do
   def day(), do: 3
 
   @impl Day
-  def a(_) do
-    :not_solved
+  def a(file) do
+    ~r/mul\((\d+),(\d+)\)/
+    |> Regex.scan(file)
+    |> Enum.map(&do_mul/1)
+    |> Enum.sum()
   end
 
   @impl Day
-  def b(_) do
-    :not_solved
+  def b(file) do
+    ~r/don't\(\).*?do\(\)/s
+    |> Regex.replace(file, "")
+    |> then(&Regex.scan(~r/mul\((\d+),(\d+)\)/, &1))
+    |> Enum.map(&do_mul/1)
+    |> Enum.sum()
   end
 
   @impl Day
   def parse_input() do
     with {:ok, file} <- Day.load(__MODULE__) do
       file
-      |> String.split("\n", trim: true)
     end
+  end
+
+  @spec do_mul([binary]) :: integer
+  defp do_mul([_ | nums]) do
+    nums
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.product()
   end
 end
